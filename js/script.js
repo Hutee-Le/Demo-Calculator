65223133aabaf3a828f3b68eddb5b8382cce2cb1
Minh Nhut <lenhut0929@gmail.com>
2023-11-18 17:21:32 +0700

    update version 1.3

--- js/script.js
+++ js/script.js
@@ -1,108 +1,96 @@
-// Lấy các phần tử từ DOM
 const display = document.getElementById('display');
 const buttons = document.querySelectorAll('.btn');
 const clearButton = document.getElementById('clear');
 const equalsButton = document.getElementById('equals');
 
-// Khai báo biến để lưu trữ dữ liệu đầu vào hiện tại, toán tử được chọn và kết quả
 let currentInput = '';
 let operator = '';
 let result = '';
+let shouldClearDisplay = false;
 
-// Thêm sự kiện click cho mỗi nút
 buttons.forEach(button => {
     button.addEventListener('click', () => handleButtonClick(button.textContent));
 });
 
-// Xử lý khi nút clear (C) và equals (=) được nhấn
 clearButton.addEventListener('click', clearDisplay);
 equalsButton.addEventListener('click', performCalculation);
 
-// Xử lý sự kiện nhấn nút
 function handleButtonClick(value) {
-    switch(value) {
-        case 'C':
-            clearDisplay();
-            break;
-        case '+/-':
-            if (currentInput !== '') {
-                currentInput = String(-parseFloat(currentInput));
-            }
-            break;
-        case '%':
-            if (currentInput !== '') {
-                currentInput = String(parseFloat(currentInput) / 100);
-            }
-            break;
-        case '.':
-            if (!currentInput.includes('.')) {
-                currentInput += value;
-            }
-            break;
-        case '=':
-            performCalculation();
-            break;
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-            setOperator(value);
-            break;
-        default:
-            if (value >= '0' && value <= '9') {
-                currentInput += value;
-            }
-            break;
+    if (shouldClearDisplay) {
+        display.value = '';
+        shouldClearDisplay = false;
     }
-    updateDisplay();
-}
-
-// Thiết lập toán tử cho phép tính
-function setOperator(op) {
-    if (currentInput === '' && result) {
-        operator = op;
-    } else if (currentInput !== '') {
-        if (operator) {
-            performCalculation();
-        } else {
-            result = currentInput;
+    if (value >= '0' && value <= '9') {
+        currentInput += value;
+        display.value += value; // Thêm giá trị số vào màn hình
+    } else if (value === '.' && !currentInput.includes('.')) {
+        currentInput += value;
+        display.value += value; // Thêm dấu chấm vào màn hình
+    } else if (value === 'C') {
+        clearDisplay();
+    } else if (value === '=') {
+        performCalculation();
+    } else if (value === '+/-') {
+        handleToggleSign();
+    } else if (value === '%') {
+        handlePercentage();
+    } else {
+        if (currentInput !== '') {
+            if (operator !== '') {
+                performCalculation();
+            } else {
+                result = currentInput;
+            }
+            operator = value;
+            currentInput = '';
         }
-        operator = op;
-        currentInput = '';
+        display.value += value; // Thêm toán tử vào màn hình
     }
 }
 
-// Cập nhật hiển thị
-function updateDisplay() {
-    display.value = `${result} ${operator} ${currentInput}`;
+
+function handleToggleSign() {
+    if (currentInput !== '') {
+        currentInput = (parseFloat(currentInput) * -1).toString();
+        display.value = currentInput;
+    }
 }
 
-// Thực hiện tính toán
+function handlePercentage() {
+    if (currentInput !== '') {
+        currentInput = (parseFloat(currentInput) / 100).toString();
+        display.value = currentInput;
+    }
+}
+
 function performCalculation() {
-    if (currentInput !== '' && operator && result) {
-        switch(operator) {
-            case '+':
-                result = String(parseFloat(result) + parseFloat(currentInput));
-                break;
-            case '-':
-                result = String(parseFloat(result) - parseFloat(currentInput));
-                break;
-            case '*':
-                result = String(parseFloat(result) * parseFloat(currentInput));
-                break;
-            case '/':
-                result = String(parseFloat(result) / parseFloat(currentInput));
-                break;
+    if (currentInput !== '') {
+        if (operator === '+') {
+            result = (parseFloat(result) + parseFloat(currentInput)).toString();
+        } else if (operator === '-') {
+            result = (parseFloat(result) - parseFloat(currentInput)).toString();
+        } else if (operator === '*') {
+            result = (parseFloat(result) * parseFloat(currentInput)).toString();
+        } else if (operator === '/') {
+            if (parseFloat(currentInput) === 0) {
+                display.value = "Error";
+                shouldClearDisplay = true;
+                return;
+            } else {
+                result = (parseFloat(result) / parseFloat(currentInput)).toString();
+            }
         }
-        currentInput = '';
-        operator = '';
     }
+    currentInput = '';
+    operator = '';
+    display.value = result;
+    shouldClearDisplay = true;
 }
 
-// Xóa hiển thị
 function clearDisplay() {
     currentInput = '';
     operator = '';
     result = '';
-    updateDisplay();
+    display.value = '';
+    shouldClearDisplay = false;
 }
